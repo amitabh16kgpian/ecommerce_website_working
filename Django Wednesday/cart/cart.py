e1bc172547f1b0029eb24e75011d7d84163d4cf3
amitabh16kgpian <95433538+amitabh16kgpian@users.noreply.github.com>
2024-02-26 15:14:06 +0530

    Delete Django Wednesday directory

--- Django Wednesday/cart/cart.py
+++
@@ -1,6 +0,0 @@
-
-
-class Cart():
-    def __init__(self, request):
-        self.session = request.session
-        self.cart = self.session.get('session_key', {})
--- Django Wednesday/cart/context_processors.py
+++
@@ -1,5 +0,0 @@
-from .cart import Cart
-
-def cart(request):
-    # return the default data from the cart
-    return {'cart': Cart(request)}
